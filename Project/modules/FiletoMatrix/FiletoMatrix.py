2d14933ad623db8ff1ffba663921616a062cb94e
martulioruiz <martulioruiz@gmail.com>
2019-08-26 01:06:26 -0600

    Proyect/modules/FiletoMatrix: 100% funcional

--- Project/modules/FiletoMatrix/FiletoMatrix.py
+++ Project/modules/FiletoMatrix/FiletoMatrix.py
@@ -4,86 +4,49 @@ content = f.read()
 
 rows = content.split("\n")
 
+tree = []
 
-def FileToMatrix(array = rows, tab = 0):
-	for row in range(len(array)):
-		column = array[row].split("\t")
-		if len(column)-1>tab :
-			print(str(len(column)-1)+" es mayor que "+str(tab))
-			print(column[len(column)-1])
-			temp = []
-			temp.append(column[len(column)-1])
-			print("matrix actual"+matrix[tab])
+def FiletoMatrix(matrix = tree):
+	matrix.append(eval('[' + FiletoMatrixInner() + ']'))
 
-			tab += 1
-			matrix[tab].append(temp)
-
-
-		elif len(column)-1<tab:
-			print(str(len(column)-1)+" es menor que "+str(tab))
-			print(column[len(column)-1])
-			tab -= 1
-		else:
-			print(str(len(column)-1)+" es igual que "+str(tab))
-			print(column[len(column)-1])
-			if tab==0:
-				matrix.append(column[len(column)-1])
-			else:
-				matrix[tab].append(column[len(column)-1])
-
-
-def toMatrix(tabs=0, contador=0 , matrix=[], array = rows):
+def FiletoMatrixInner(tabs=0, contador=0 , temp=[], array = rows):
 	if contador < len(array):
 		column = array[contador].split("\t")
 		string = column[len(column)-1]
-		
+
 		if len(column)-1 < tabs:
 			if string[len(string)-1] == "/":
-				print(string)
-				matrix.append(string)
-				matrix.append([])
-				toMatrix(tabs-1,contador+1,matrix[1])
+				return "],"+"'"+str(string)+"'"+",["+str(FiletoMatrixInner(tabs-1,contador+1))
 			else:
-				matrix.append(string)
-				toMatrix(tabs-1,contador+1,matrix)
+				return "],"+"'"+str(string)+"'"+","+str(FiletoMatrixInner(tabs,contador+1))
 
-
-		elif len(column) > tabs:
+		elif len(column)-1 > tabs:
 			if string[len(string)-1] == "/":
-				print(string)
-				matrix.append(string)
-				matrix.append([])
-				toMatrix(tabs+1,contador+1,matrix[1])
+				return "'"+str(string)+"'"+",["+str(FiletoMatrixInner(tabs+1,contador+1))
 			else:
-				matrix.append(string)
-				toMatrix(tabs+1,contador+1,matrix)
-
+				return "'"+str(string)+"'"+","+str(FiletoMatrixInner(tabs,contador+1))	
 
 		else:
 			if string[len(string)-1] == "/":
-				print(string)
-				matrix.append(string)
-				matrix.append([])
-				toMatrix(tabs+1,contador+1,matrix[1])
+				return "'"+str(string)+"'"+",["+str(FiletoMatrixInner(tabs+1,contador+1))
 			else:
-				matrix.append(string)
-				toMatrix(tabs,contador+1,matrix)
+				return "'"+str(string)+"'"+","+str(FiletoMatrixInner(tabs,contador+1))
  
-	else:
-		print("fin")
-		return matrix
+	return "]"
 
 
-	
+FiletoMatrix(tree)
+tree2 = tree[0]
+print()
 
+print(tree2)
 
+print()
 
-
-
-
-
-
-matrix = toMatrix()
-print(matrix)
+print(tree2[1][0])
+print(tree2[1][1][1][1])
+print(tree2[1][3][2])
+print(tree2[1][4])
+print(tree2[1][2])
 
 f.close()
